7eb819274d47d828b7ac8b98c34206078056e422
xHelpy <xiozeerpl@gmail.com>
2024-11-06 08:26:36 +0100

    adding files

---
+++ kotyserializacja/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kotyserializacja
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kotyserializacja/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Common;
+using static System.Net.WebRequestMethods;
+
+namespace kotyserializacja
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            string Link = "https://cataas.com/cat";
+        }
+
+        async private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string Link = "https://cataas.com/cat";
+            
+            
+            
+
+                Cat.Source = Link;
+            
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string Link = "https://cataas.com/cat/says/" + input.Text;
+
+
+            Cat.Source = Link;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            string Link = "https://cataas.com/cat/gif";
+
+            Cat.Source = Link;
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            string Link = "https://cataas.com/cat/gif/says/" + input2.Text;
+
+
+            Cat.Source = Link;
+        }
+    }
+
+}
